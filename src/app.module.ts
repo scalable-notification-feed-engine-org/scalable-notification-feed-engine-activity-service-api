c9dc53c69428de91704d37082aa3d36047fb116a
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-14 21:13:34 +0530

    feat(app-module): integrate tenant middleware and global persistence layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActivityModule } from './modules/activity/activity.module';
+import { TenantMiddleware } from './common/middleware/tenant.middleware';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ActivityModule],
+  imports: [ActivityModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TenantMiddleware).forRoutes('*');
+  }
+}
