b61e30913b82c24767ca6347c25bb7a23cccad3d
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-07-04 10:41:50 +0530

    fix(activity): add PrismaService to providers for dependency injection

--- src/modules/activity/activity.module.ts
+++ src/modules/activity/activity.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
-import { PrismaService } from '../../prisma/prisma.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   imports: [
@@ -23,6 +23,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     ]),
   ],
   controllers: [ActivityController],
-  providers: [ActivityService],
+  providers: [ActivityService, PrismaService],
 })
 export class ActivityModule {}
